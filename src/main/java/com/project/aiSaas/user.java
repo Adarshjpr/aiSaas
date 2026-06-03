854e8a07f62c39f9b02a0167547132bf027e196c
Adarshjpr <adarshjpr7@gmail.com>
2026-06-03 11:55:05 +0530

    jpa

--- src/main/java/com/project/aiSaas/user.java
+++ src/main/java/com/project/aiSaas/user.java
@@ -1,11 +1,23 @@
 package com.project.aiSaas;
 
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class user {
+
+ 
+    private final JdbcTemplate jdbcT ;
+    
+    //  CONSTRUCTORE INJECT 
+     user(JdbcTemplate jdbcT  ){
+        this.jdbcT = jdbcT; 
+     }
 
  @GetMapping("/welcome")
     public String welcome(){
@@ -13,4 +25,20 @@ public class user {
         return" welcome vivek" ;
     }
  
+    
+
+    @PostMapping("/register")
+    public userModel postMethodName(@RequestBody userModel user) {
+        
+        //  sql queary 
+  String sql = "INSERT INTO users(user_name, numbers, email_id , passwords) VALUES (?, ?, ?, ?)";
+
+  jdbcT.update(sql  , user.getName() ,user.getNumbers() , user.getMail() , user.getPasswords());
+  System.out.println(user.getName() );
+  System.out.println(user.getPasswords());
+        return user;
+    }
+    
+
+    
 }
---
+++ src/main/java/com/project/aiSaas/userModel.java
@@ -0,0 +1,35 @@
+package com.project.aiSaas;
+
+public class userModel {
+
+ 
+  private String name;
+  private String mail;
+  private String  passwords;
+  private String  numbers;
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public String getMail() {
+    return mail;
+}
+public void setMail(String mail) {
+    this.mail = mail;
+}
+public String getPasswords() {
+    return passwords;
+}
+public void setPasswords(String passwords) {
+    this.passwords = passwords;
+}
+public String getNumbers() {
+    return numbers;
+}
+public void setNumbers(String numbers) {
+    this.numbers = numbers;
+}
+
+}
