6b295d0c8e8bcf5b889b9a1ed98fd0d9f64549f7
Adarshjpr <adarshjpr7@gmail.com>
2026-06-03 13:22:26 +0530

    Repository

--- src/main/java/com/project/aiSaas/userModel.java
+++ src/main/java/com/project/aiSaas/userModel.java
@@ -1,12 +1,31 @@
 package com.project.aiSaas;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class userModel {
 
- 
-  private String name;
-  private String mail;
-  private String  passwords;
-  private String  numbers;
+//   koi bhi table bana rahe ho to usme primary key 
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int id ;
+
+
+@Column(name = "usernames" )
+  private String name;    //   usernams varchar(255)
+  @Column(unique = true )
+  private String mail;     // mail varchar(255) unquie 
+  @Column(nullable = false)
+  private String  passwords;  // passwords  varchar(255)  not null
+  private String  numbers;    //  numbers varchar(255)
+
+  private String  gender;
 public String getName() {
     return name;
 }
