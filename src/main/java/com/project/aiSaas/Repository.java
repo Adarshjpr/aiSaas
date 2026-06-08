38f7d9f3b20d5dc04c9f290f427f5322731dbacf
Adarshjpr <adarshjpr7@gmail.com>
2026-06-08 12:27:42 +0530

    get delete

--- src/main/java/com/project/aiSaas/Repository.java
+++ src/main/java/com/project/aiSaas/Repository.java
@@ -2,7 +2,6 @@ package com.project.aiSaas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Repository  extends JpaRepository<userModel , Integer >  {
-
-    //  save update , delete  bahut sari  method free me 
+public interface Repository extends JpaRepository<userModel , Integer >  {
+    
 }
--- src/main/java/com/project/aiSaas/user.java
+++ src/main/java/com/project/aiSaas/user.java
@@ -1,11 +1,18 @@
 package com.project.aiSaas;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -45,6 +52,24 @@ repository.save(user);
         return user;
     }
     
+
+//  get api 
+
+@GetMapping("/get/{id}")
+public List<userModel> getMethodName(  @PathVariable Integer id ) {
+   
+ return  repository.findAll();
+
+}
+
+
+@DeleteMapping("/delete/{id}")
+public String  Deletedata(@PathVariable  Integer id){
+  
+repository.deleteById(id);
+
+  return "success fulli  delete " ;
+} 
 
     
 }
